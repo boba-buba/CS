eca55e573d472af2cff1498221064c1a5b9ec732
Nataliia Korop <n.corop08@gmail.com>
2023-10-11 18:30:46 +0200

    WordCounting and WordFrequency pretty

--- WordFrequency/Program.cs
+++ WordFrequency/Program.cs
@@ -5,29 +5,45 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Linq;
+using System.Collections;
 
-namespace wordCounting
+namespace WordFrequency
 {
-
     class FileInfo
     {
-        public FileInfo() { wordCount = 0; }
+        public FileInfo() {  }
 
-        private int wordCount;
-        public void PrintWordCount() { Console.WriteLine(wordCount); }
-        public void IncrementWordCount() { wordCount++; }
-        
+        Dictionary<string, int> words = new Dictionary<string, int>(); 
+        public void PrintWords() 
+        {
+            foreach (KeyValuePair<string, int> pair in words.OrderBy(pair => pair.Key))
+            {
+                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+            }
+        }
+
+        public void AddToDict(string word) 
+        {
+            if (words.ContainsKey(word))
+            {
+                words[word]++;
+            }
+            else
+            {
+                words.Add(word, 1);
+            }
+        }
     }
 
     class FileParser
     {
-        public FileParser(FileInfo fi) { info = fi;  }
+        public FileParser(FileInfo fi) { info = fi; }
         FileInfo info;
         public void ParseFile(string fileName)
         {
             char ch;
             string word = "";
-            char[] whiteChars = { ' ', '\n', '\t' , '\r'};
+            char[] whiteChars = { ' ', '\n', '\t', '\r' };
             int charInt = 0;
 
             StreamReader reader = new StreamReader(fileName);
@@ -38,46 +54,47 @@ namespace wordCounting
                 ch = (char)charInt;
                 if (!(whiteChars.Contains(ch)))
                 {
-                    word += ch;   
+                    word += ch; 
                 }
                 else if ((whiteChars.Contains(ch)) && (word.Length > 0))
                 {
-                    info.IncrementWordCount();
-                    word = "";                
-                }                
+                    info.AddToDict(word);
+                    word = "";
+                }
                 charInt = reader.Read();
             }
-            if (word.Length > 0) { info.IncrementWordCount(); }
-            
+            if (word.Length > 0) { info.AddToDict(word); }
+
             reader.Close();
             reader.Dispose();
         }
     }
 
-   
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1) {
+            if (args.Length != 1)
+            {
                 Console.WriteLine("Argument Error");
                 return;
             }
 
             try
             {
-                string f = args[0]; 
+                string f = args[0];
                 FileInfo fi = new FileInfo();
                 FileParser fp = new FileParser(fi);
                 fp.ParseFile(f);
-                fi.PrintWordCount();
+                fi.PrintWords();
             }
             catch (IOException)
             {
                 Console.WriteLine("File Error");
             }
-            
+
         }
     }
 }
